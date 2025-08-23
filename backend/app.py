9a2fedeca2f98f46082b869980addf1a5d8e7939
Yaseen_Shaikh <yaseenmaqboolshaikh@gmail.com>
2025-08-23 17:48:10 +0530

    Update app.py with resolved conflicts and FAQ DB functionality

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,5 @@
 from flask import Flask, request, jsonify
-<<<<<<< HEAD
-=======
 import sqlite3
->>>>>>> db-dev
 
 app = Flask(__name__)
 
@@ -10,8 +7,6 @@ app = Flask(__name__)
 def home():
     return "Server running"
 
-<<<<<<< HEAD
-=======
 def find_answer(query):
     conn = sqlite3.connect("database/faq.db")
     cur = conn.cursor()
@@ -20,21 +15,16 @@ def find_answer(query):
     conn.close()
     return row[0] if row else None
 
->>>>>>> db-dev
 @app.route("/ask", methods=["POST"])
 def ask():
     data = request.json
     query = data.get("query", "")
-<<<<<<< HEAD
-    return jsonify({"response": f"You asked: {query}. (DB/AI coming soon)"})
-=======
 
     answer = find_answer(query)
     if answer:
         return jsonify({"response": answer})
     else:
         return jsonify({"response": "Sorry, I couldn’t find this in FAQs. (AI fallback coming soon)"})
->>>>>>> db-dev
 
 if __name__ == "__main__":
     app.run(debug=True)
